79af7e88e1901d230750d7af66a90519f845f441
dlgusals97 <59655095+Lee-Hyeon-Min@users.noreply.github.com>
2022-11-26 20:32:00 +0900

    Update Smart_Plant_Active.ino

--- Smart_Plant_Active.ino
+++ Smart_Plant_Active.ino
@@ -2,22 +2,22 @@
 #include <LiquidCrystal_I2C.h>
 #include <DHT.h>
 
-int pump = 13;
+int pump = 13; // PORTB의 5번 => PB |= 0x20
 
 void setup() {
   Serial.begin(9600);
-  pinMode(pump,OUTPUT);
+  pinMode(pump,OUTPUT); // 13번 pin output mode
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   char data_rcvd = Serial.read();
   if(data_rcvd == '1'){
-    digitalWrite(pump,LOW);
+    digitalWrite(pump,LOW); // 13번 pin 나중에 |= 연산 진행
     delay(1000);
   }
   else if(data_rcvd == '0'){
-    digitalWrite(pump,HIGH);
+    digitalWrite(pump,HIGH); // 13번 pin 나중에 |= 연산 진행
     delay(1000);  
   }
 }
